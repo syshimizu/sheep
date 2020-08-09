996c771ad8ba1134675888489527ad7bfe6b130a
syshimizu <soonyoungshimizu@gmail.com>
2020-08-08 22:48:01 -0400

    footer changes

--- Sheep/generate_sheep_list.py
+++ Sheep/generate_sheep_list.py
@@ -1,7 +1,7 @@
 """
 Prints out a list of filenames in strings, separated by columns in terminal. 
 Copy and paste into sheep_list.js (within brackets[])
-
+Creates a .csv file with all filenames
 """
 
 import glob
@@ -20,29 +20,14 @@ for n in files:
 
 print(",".join(list(map(lambda file: "\"" + file + "\"",files))))
 
+### Not needed but for easy reference of all sheep files, exports a .csv of all file names
 
-
-
-""" 
 df = pd.DataFrame(files)
-
-#exporting data frame to .csv
-export = df.to_json(r'./sheep_list.json')
-print('Merging complete')
-
-
-# Combining all files into one data frame
-data = pd.DataFrame()
-for n in files:
-    i = files.index(n)
-    df = pd.read_csv(files[i])
-    data = data.append(df)
 
 # Receiving today's date
 currentDT = datetime.datetime.now()
 date = currentDT.strftime("_%Y_%m_%d")
 
 # Exporting data frame as a .csv with today's date appended. 
-export = data.to_csv(r'./Master_data'+ date +'.csv', index = None, header=True)
+export = df.to_csv(r'./Master_data'+ date +'.csv', index = None, header=True)
 print('Merging complete')
-"""
